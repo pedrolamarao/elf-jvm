56dce6ea9743f53e13205543c9076ec5cf2451b0
Pedro Lamarão <pedro.lamarao@gmail.com>
2022-04-13 19:27:27 -0300

    Define ELF views up to symbol tables.

--- api/src/main/kotlin/br/dev/pedrolamarao/elf/elf.kt
+++ api/src/main/kotlin/br/dev/pedrolamarao/elf/elf.kt
@@ -1,55 +1,113 @@
 package br.dev.pedrolamarao.elf
 
 import java.nio.ByteBuffer
-import java.nio.channels.FileChannel
-import java.nio.file.Path
 
-interface ElfPrologue
+class ElfFileView (private val bytes : ByteBuffer)
 {
-    fun type () : UByte
+    fun magic (): ByteBuffer = bytes.slice(0,4).order(bytes.order())
+
+    fun format () = bytes[4]
+
+    fun encoding () = bytes[5]
+
+    fun version () = bytes[6]
+
+    fun abi () = bytes[7]
+
+    fun abiVersion () = bytes[8]
 }
 
-interface ElfImage32 : ElfPrologue
+class ElfFileView64 (private val bytes : ByteBuffer)
 {
-    fun entry () : UInt
+    fun type () = bytes.getShort(16)
+
+    fun machine () = bytes.getShort(18)
+
+    fun version () = bytes.getInt(20)
+
+    fun entryAddress () = bytes.getLong(24)
+
+    fun segmentTableOffset () = bytes.getLong(32)
+
+    fun sectionTableOffset () = bytes.getLong(40)
+
+    fun flags () = bytes.getInt(48)
+
+    fun size () = bytes.getShort(52)
+
+    fun segmentEntrySize () = bytes.getShort(54)
+
+    fun segmentTableSize () = bytes.getShort(56)
+
+    fun sectionEntrySize () = bytes.getShort(58)
+
+    fun sectionTableSize () = bytes.getShort(60)
+
+    fun sectionNameIndex () = bytes.getShort(62)
 }
 
-interface ElfImage64 : ElfPrologue
+class ElfSectionView64 (private val bytes : ByteBuffer)
 {
-    fun entry () : ULong
+    fun name () = bytes.getInt(0)
+
+    fun type () = bytes.getInt(4)
+
+    fun flags () = bytes.getLong(8)
+
+    fun address () = bytes.getLong(16)
+
+    fun offset () = bytes.getLong(24)
+
+    fun size () = bytes.getLong(32)
+
+    fun link () = bytes.getInt(40)
+
+    fun information () = bytes.getInt(44)
+
+    fun alignment () = bytes.getLong(48)
+
+    fun entrySize () = bytes.getLong(56)
 }
 
-class ElfImageView32 (private val bytes : ByteBuffer) : ElfImage32
+class ElfSymbolView64 (private val bytes : ByteBuffer)
 {
-    override fun type () = bytes[4].toUByte()
+    fun name () = bytes.getInt(0)
 
-    override fun entry () = bytes.getInt(28).toUInt()
+    fun information () = bytes.get(4)
+
+    fun other () = bytes.get(5)
+
+    fun section () = bytes.getShort(6)
+
+    fun value () = bytes.getLong(8)
+
+    fun size () = bytes.getLong(16)
 }
 
-class ElfImageView64 (private val bytes : ByteBuffer) : ElfImage64
+class ElfSectionTableView64 (private val bytes : ByteBuffer, private val size : Int) : Iterable<ElfSectionView64>
 {
-    override fun type () = bytes[4].toUByte()
-
-    override fun entry () = bytes.getInt(28).toULong()
+    override fun iterator() = ElfSectionTableViewIterator64(bytes, size, 0)
 }
 
-class ElfParser
+class ElfSectionTableViewIterator64 (private val bytes : ByteBuffer, private val size : Int, private var index : Int) : Iterator<ElfSectionView64>
 {
-    fun view (path : Path) : ElfPrologue?
-    {
-        FileChannel.open(path).use {
-            val bytes = it.map(FileChannel.MapMode.READ_ONLY, 0, 16)
-            val isElf = (bytes.remaining() == 16)
-                && (bytes[0] == 0x7F.toByte())
-                && (bytes[1] == 'E'.code.toByte())
-                && (bytes[2] == 'L'.code.toByte())
-                && (bytes[3] == 'F'.code.toByte())
-            if (! isElf) return null
-            else return when (bytes[4]) {
-                1.toByte() -> ElfImageView32(bytes)
-                2.toByte() -> ElfImageView64(bytes)
-                else -> null
-            }
-        }
-    }
+    override fun hasNext () = size*index < bytes.remaining()
+
+    override fun next () = ElfSectionView64(
+        bytes.slice(size*index++,size).order(bytes.order())
+    )
+}
+
+class ElfSymbolTableView64 (private val bytes : ByteBuffer, private val size : Int) : Iterable<ElfSymbolView64>
+{
+    override fun iterator() = ElfSymbolTableViewIterator64(bytes, size, 0)
+}
+
+class ElfSymbolTableViewIterator64 (private val bytes : ByteBuffer, private val size : Int, private var index : Int) : Iterator<ElfSymbolView64>
+{
+    override fun hasNext () = size*index < bytes.remaining()
+
+    override fun next () = ElfSymbolView64(
+        bytes.slice(size*index++,size).order(bytes.order())
+    )
 }
--- api/src/test/kotlin/br/dev/pedrolamarao/elf/test.kt
+++ api/src/test/kotlin/br/dev/pedrolamarao/elf/test.kt
@@ -1,17 +1,80 @@
-import br.dev.pedrolamarao.elf.ElfImage64
-import br.dev.pedrolamarao.elf.ElfParser
+import br.dev.pedrolamarao.elf.ElfFileView
+import br.dev.pedrolamarao.elf.ElfFileView64
+import br.dev.pedrolamarao.elf.ElfSectionTableView64
+import br.dev.pedrolamarao.elf.ElfSymbolTableView64
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.fail
+import java.nio.ByteBuffer
+import java.nio.ByteOrder.LITTLE_ENDIAN
+import java.nio.channels.FileChannel
+import java.nio.channels.FileChannel.MapMode.READ_ONLY
 import java.nio.file.Paths
+
+private val magic = arrayOf<Byte>(0x7F, 0x45, 0x4C, 0x46).toByteArray()
 
 class ParserTest
 {
     @Test
     fun smoke ()
     {
-        val prologue = ElfParser().view( Paths.get( System.getProperty("br.dev.pedrolamarao.elf.sample.path") ) )
-        if (prologue !is ElfImage64) { fail("not an ElfImage32") }
-        assertEquals(2.toUByte(),prologue.type())
+        FileChannel.open( Paths.get( System.getProperty("br.dev.pedrolamarao.elf.sample.path") ) ).use{
+            file ->
+            val prologue = ElfFileView( file.map(READ_ONLY,0,16).order(LITTLE_ENDIAN) )
+            assertEquals(ByteBuffer.wrap(magic),prologue.magic())
+            assertEquals(2.toByte(),prologue.format())
+            assertEquals(1.toByte(),prologue.encoding())
+            assertEquals(1.toByte(),prologue.version())
+            assertEquals(0.toByte(),prologue.abi())
+            assertEquals(0.toByte(),prologue.abiVersion())
+            val header = ElfFileView64( file.map(READ_ONLY,0,64).order(LITTLE_ENDIAN) )
+            assertEquals(2.toShort(),header.type())
+            assertEquals(62.toShort(),header.machine())
+            assertEquals(1,header.version())
+            header.entryAddress()
+            header.segmentTableOffset()
+            header.sectionTableOffset()
+            header.flags()
+            header.size()
+            header.segmentEntrySize()
+            header.segmentTableSize()
+            header.sectionEntrySize()
+            header.sectionTableSize()
+            header.sectionNameIndex()
+            val sectionTable = ElfSectionTableView64(
+                file.map(
+                    READ_ONLY,
+                    header.sectionTableOffset(),
+                    (header.sectionEntrySize() * header.sectionTableSize()).toLong()
+                )
+                .order(LITTLE_ENDIAN),
+                header.sectionEntrySize().toInt()
+            )
+            val symbolTables = mutableListOf<ElfSymbolTableView64>()
+            for (section in sectionTable) {
+                section.name()
+                section.type()
+                section.flags()
+                section.address()
+                section.offset()
+                section.size()
+                section.link()
+                section.information()
+                section.alignment()
+                section.entrySize()
+                val bytes = file.map(READ_ONLY, section.offset(), section.size()).order(LITTLE_ENDIAN)
+                if (section.type() == 2)
+                    symbolTables.add( ElfSymbolTableView64(bytes,section.entrySize().toInt()) )
+            }
+            for (symbolTable in symbolTables) {
+                for (symbol in symbolTable) {
+                    symbol.name()
+                    symbol.information()
+                    symbol.other()
+                    symbol.section()
+                    symbol.value()
+                    symbol.size()
+                }
+            }
+        }
     }
 }
